6df6648343d191b3166529bddd25707a746e0236
Abdul Rauf <hafizabdul.rauf@dnata.com>
2021-11-17 19:01:49 +0400

    - initial commit

---
+++ CoffeeExplorer/CoffeeExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeExplorerApp.swift
+//  CoffeeExplorer
+//
+//  Created by ar on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeExplorerUITests/CoffeeExplorerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CoffeeExplorerUITestsLaunchTests.swift
+//  CoffeeExplorerUITests
+//
+//  Created by ar on 17/11/2021.
+//
+
+import XCTest
+
+class CoffeeExplorerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
